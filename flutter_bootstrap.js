242e5145581be26aa9379b373128c38511b26bc3
SrikantaPrasadrajeUrs <srikantaprasadrajeurs456@gmail.com>
2025-01-01 20:26:45 +0530

    fixed card errors

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1871159516"
+    serviceWorkerVersion: "2846136648"
   }
 });
